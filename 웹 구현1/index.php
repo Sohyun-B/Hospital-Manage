3754df53fe5ef9b7936419bd9bc15e0864db1e1f
Seohyun Ryu <79790536+ruruisryu@users.noreply.github.com>
2023-05-27 17:36:37 +0900

    Feat: index.php 추가 (#4)
    * feat: index.php 추가, 웹구현 필요없는 것 삭제

---
+++ 웹 구현1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1> EWHA HOSPITAL MANAGEMENT SYSTEM  </h1>
+        <h2> 곽선호 김민영 배소현 유서현 이나영 </h2>
+
+        <h3> <a href = "Patient_Info.php"> 환자병원 진료정보 </a> </h3>
+        <h3> <a href = "2.php"> 분석 보고서 </a></h3>
+    </body>
+</html>
+    
